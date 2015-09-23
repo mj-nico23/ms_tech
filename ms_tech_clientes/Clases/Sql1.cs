075cacb17811dc223a399979d79bfe207df84d9d
mj-nico23 <nicograglia@gmail.com>
2015-09-23 13:01:14 -0300

    s

--- ms_tech_clientes/Clases/Sql1.cs
+++ ms_tech_clientes/Clases/Sql1.cs
@@ -9,7 +9,8 @@ namespace ms_tech_clientes
 {
     public class Sql1
     {
-        static string cnn_string = "data source=NYC;initial catalog=ms_tech;user id=sa;password=12345;";
+        //static string cnn_string = "data source=NYC;initial catalog=ms_tech;user id=sa;password=12345;";
+        static string cnn_string = "data source=nb-nico2\\sqlexpress;initial catalog=ms_tech;user id=sa;password=12345;";
         public static DataTable Execute(string sql)
         {
             DataTable dt = new DataTable();
